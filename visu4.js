cb19180e140aac36410e676cb99ef108de3d6eba
BOUMAZA TARIK p1712760 tarik.boumaza@etu.univ-lyon1.fr <tarik.boumaza@etu.univ-lyon1.fr>
2022-01-05 13:55:16 +0100

    modif print bulles

--- visu4.js
+++ visu4.js
@@ -27,34 +27,34 @@ function BubbleChart(data, {
     stroke = "black", // a static stroke around the bubbles
     strokeWidth = ".3px", // the stroke width around the bubbles, if any
     strokeOpacity, // the stroke opacity around the bubbles, if any
-  } = {}) {
+} = {}) {
     // Compute the values.
     const D = d3.map(data, d => d);
     const V = d3.map(data, value);
     const G = group == null ? null : d3.map(data, group);
     const I = d3.range(V.length).filter(i => V[i] > 0);
-  
+
     // Unique the groups.
     if (G && groups === undefined) groups = I.map(i => G[i]);
     groups = G && new d3.InternSet(groups);
-  
+
 
     // Construct scales.
     const color = G && d3.scaleQuantize(groups, colors)
-                        .domain([0, 100])
-                        .range(d3.schemeRdBu[7]);
-  
+        .domain([0, 100])
+        .range(d3.schemeRdBu[7]);
+
     // Compute labels and titles.
     const L = label == null ? null : d3.map(data, label);
     const T = title === undefined ? L : title == null ? null : d3.map(data, title);
-  
+
     // Compute layout: create a 1-deep hierarchy, and pack it.
     const root = d3.pack()
         .size([width - marginLeft - marginRight, height - marginTop - marginBottom])
         .padding(padding)
-      (d3.hierarchy({children: I})
-        .sum(i => V[i]));
-  
+        (d3.hierarchy({ children: I })
+            .sum(i => V[i]));
+
     const svg = d3.select("#visu4")
         .append("svg")
         .attr("width", width)
@@ -65,14 +65,14 @@ function BubbleChart(data, {
         .attr("font-size", 10)
         .attr("font-family", "sans-serif")
         .attr("text-anchor", "middle");
-  
+
     const leaf = svg.selectAll("a")
-      .data(root.leaves())
-      .join("a")
+        .data(root.leaves())
+        .join("a")
         .attr("xlink:href", link == null ? null : (d, i) => link(D[d.data], i, data))
         .attr("target", link == null ? null : linkTarget)
         .attr("transform", d => `translate(${d.x},${d.y})`);
-  
+
     leaf.append("circle")
         .attr("stroke", stroke)
         .attr("stroke-width", strokeWidth)
@@ -80,21 +80,21 @@ function BubbleChart(data, {
         .attr("fill", G ? d => color(G[d.data]) : fill == null ? "none" : fill)
         .attr("fill-opacity", fillOpacity)
         .attr("r", d => d.r);
-  
+
     if (T) leaf.append("title")
         .text(d => T[d.data]);
-  
+
     if (L) {
-      // A unique identifier for clip paths (to avoid conflicts).
-      const uid = `O-${Math.random().toString(16).slice(2)}`;
-  
-      leaf.append("clipPath")
-          .attr("id", d => `${uid}-clip-${d.data}`)
-        .append("circle")
-          .attr("r", d => d.r);
-  
-      leaf.append("text")
-          .attr("clip-path", d => `url(${new URL(`#${uid}-clip-${d.data}`, location)})`)
+        // A unique identifier for clip paths (to avoid conflicts).
+        const uid = `O-${Math.random().toString(16).slice(2)}`;
+
+        leaf.append("clipPath")
+            .attr("id", d => `${uid}-clip-${d.data}`)
+            .append("circle")
+            .attr("r", d => d.r);
+
+        leaf.append("text")
+            .attr("clip-path", d => `url(${new URL(`#${uid}-clip-${d.data}`, location)})`)
         .selectAll("tspan")
         .data(d => `${L[d.data]}`.split(/\n/g))
         .join("tspan")
@@ -118,8 +118,19 @@ d3.json("../data/favorites_color_level.json").then(function(data) {
         label: d =>  d.Title,
         value: d => d.Duration,
         group: d => d.ColorLevel,
-        title: d => `${d.Title}\n${d.Duration/60|0}h${d.Duration%60|0}`,
+        title: d => printbulle(d.Title, d.Duration),
         link: d => `https://www.netflix.com/${d.Title.replace(/ /g,"")}`,
         width: 600
     })
 });
+
+
+function printbulle(title, duration) {
+  if (duration < 60) {
+      return title + ": " + (Math.round(duration % 60 * 100) / 100) + "min"
+  }
+  if (duration % 60 < 10) {
+      return title + ": " + Math.round(duration / 60) + "h0" + Math.round(Math.round(duration % 60 * 100) / 100) + "min"
+  }
+  return title + ": " + Math.round(duration / 60) + "h" + Math.round(Math.round(duration % 60 * 100) / 100) + "min"
+}
