b666894fd4cf6ebb0a346e4aa08c8072a1ceea92
BOUMAZA TARIK p1712760 tarik.boumaza@etu.univ-lyon1.fr <tarik.boumaza@etu.univ-lyon1.fr>
2022-01-08 17:11:04 +0100

    commentaires

--- js/visu2.js
+++ js/visu2.js
@@ -1,10 +1,9 @@
 const width_visu2 = 400,
-    height_visu2 = 400;
+    height_visu2 = 400,
+    labelHeight = 18,
+    radius = 90;
 
-const labelHeight = 18
-
-const radius = 90;
-
+//définition du svg_visu2
 const svg_visu2 = d3.select("#visu2")
     .append("svg")
     .attr("width", width_visu2)
@@ -12,7 +11,7 @@ const svg_visu2 = d3.select("#visu2")
     .append("g")
     .attr("transform", "translate(" + 100 + "," + 250 + ")");
 
-
+// ajout titre
 svg_visu2.append("text")
     .attr("x", radius - 150)
     .attr("y", radius + 40)
@@ -20,7 +19,6 @@ svg_visu2.append("text")
     .style("font-size", "15px")
     .style("text-decoration", "underline")
     .text("Figure 4");
-
 svg_visu2.append("text")
     .attr("x", radius + 45)
     .attr("y", radius + 40)
@@ -28,38 +26,43 @@ svg_visu2.append("text")
     .style("font-size", "14px")
     .text(": Répartition du temps de visionnage par appareil");
 
-//.attr("transform", `translate(${radius},${radius})`);
-
+//choix des couleurs
 const color = d3.scaleOrdinal()
     .range(["#66a61e", "#e6ab02"])
 
+//définition de la fonction qui permet de définir les proportions
 const pie = d3.pie()
     .value(function(d) { return d[1] })
 
 let data_visu2 = {};
-let legend;
 
 d3.json("data/device_type.json").then(function(json) {
 
-    data_visu2 = json
-
-    data_plot_visu2 = pie(Object.entries(data_visu2[user]))
-
-    svg_visu2
-        .selectAll(null)
+    /*** Jeu de données ***/
+    data_visu2 = json;
+    //sélection du profil
+    data_plot_visu2 = pie(Object.entries(data_visu2[user]));
+    //dessin
+    svg_visu2.selectAll(null)
         .data(data_plot_visu2)
         .join('path')
+        //définition de l'arc avec la taille
         .attr('d', d3.arc()
             .innerRadius(0)
             .outerRadius(radius)
         )
         .attr('fill', function(d) { return (color(d.data[1])) })
+        // contour 
         .attr("stroke", "black")
         .style("stroke-width", "0.3px")
         .style("opacity", 1)
 
 
-    const size = 20
+    /*** LÉGENDE ***/
+
+    const size = 20;
+
+    // Ajout carré couleur
     svg_visu2.selectAll(null)
         .data(data_plot_visu2)
         .join("rect")
@@ -67,9 +70,9 @@ d3.json("data/device_type.json").then(function(json) {
         .attr("y", function(d, i) { return (i * (size + 5) - 60) })
         .attr("width", size)
         .attr("height", size)
-        .style("fill", function(d) { return color(d) })
+        .style("fill", function(d) { return color(d) });
 
-    // Add one dot in the legend for each name.
+    // Ajout texte couleur
     svg_visu2.selectAll(null)
         .data(data_plot_visu2)
         .join("text")
@@ -78,20 +81,24 @@ d3.json("data/device_type.json").then(function(json) {
         .style("fill", function(d) { return color(d) })
         .text(function(d) { return d.data[0] })
         .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
+        .style("alignment-baseline", "middle");
 
 });
 
 
+// mise à jour pour changement de profil
 async function update_visu2() {
+
+    //récupération du choix (profil sélectionné)
     user = Array.from(document.getElementsByName("inlineRadioOptions")).find(r => r.checked).value;
+    //récupération des données associées au profil sélectionné
+    data_plot_visu2 = pie(Object.entries(data_visu2[user]));
 
-    data_plot_visu2 = pie(Object.entries(data_visu2[user]))
-
+    //effet visuel
     await new Promise(resolve => setTimeout(resolve, 800));
 
-    svg_visu2
-        .selectAll(null)
+    //dessin du nouveau chart
+    svg_visu2.selectAll(null)
         .data(data_plot_visu2)
         .join('path')
         .attr('d', d3.arc()
--- js/visu3.js
+++ js/visu3.js
@@ -1,9 +1,9 @@
-// set the dimensions and margins of the graph
+// définition des dimensions
 const margin_visu3 = { top: 100, right: 230, bottom: 50, left: 120 },
     width_visu3 = 770 - margin_visu3.left - margin_visu3.right,
     height_visu3 = 400 - margin_visu3.top - margin_visu3.bottom;
 
-// append the svg object to the body of the page
+// ajout du svg
 const svg_visu3 = d3.select("#visu3")
     .append("svg")
     .attr("width", width_visu3 + margin_visu3.left + margin_visu3.right)
@@ -12,51 +12,40 @@ const svg_visu3 = d3.select("#visu3")
     .attr("transform",
         `translate(${margin_visu3.left}, ${margin_visu3.top})`);
 
-// Parse the Data
+// ouverture du jeu de données
 d3.csv("data/week_activity.csv").then(function(data) {
 
-
-    //////////
-    // GENERAL //
-    //////////
-
-    // List of groups = header of the csv files
     const keys = data.columns.slice(1)
 
-    // color palette
+    // définition de la fonction color
     const color = d3.scaleOrdinal()
         .domain(keys)
-        //.range(d3.schemeRdBu[3])
-        //.range(d3.schemeTableau10);
         .range(["#286EAC", "#EF6464", "#D2C8C8"])
 
-    //stack the data?
+    // fonction de cumul
     const stackedData = d3.stack()
         .keys(keys)
         (data)
 
 
-    //////////
-    // AXIS //
-    //////////
+    /************ AXES ET TITRE ***********/
 
-    // Add X axis
+    /*** Axe X ***/
+
+    // Définition
     const x = d3.scaleLinear()
         .domain(d3.extent(data, function(d) { return parseInt(d["Week"]); }))
         .range([0, width_visu3]);
 
-
+    // Ajout des labels semaines
     const nb_ticks = 6;
-
     let ticks = []
     let tickLabels = []
     for (i = 0; i <= nb_ticks; i = i + 1) {
         ticks.push(Math.round(i * week_days.length / nb_ticks));
         tickLabels.push(week_days[Math.round(i * week_days.length / nb_ticks)]);
     }
-
     tickLabels[tickLabels.length - 1] = week_days[week_days.length - 1]
-
     const xAxis = svg_visu3.append("g")
         .attr("transform", `translate(0, ${height_visu3})`)
         .call(d3
@@ -65,6 +54,34 @@ d3.csv("data/week_activity.csv").then(function(data) {
             .tickSize(3)
             .tickValues(ticks)
             .tickFormat(function(d, i) { return tickLabels[i] }));
+
+    // Label de l'axe X:
+    svg_visu3.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width_visu3 + 40)
+        .attr("y", height_visu3 + 35)
+        .style("font-size", "12px")
+        .text("Semaine");
+
+    /*** Axe Y ***/
+    let max_value = d3.max(data, function(d) { return (parseInt(d["Both"]) + parseInt(d["Hana"]) + parseInt(d["Tarik"])); })
+    const y = d3.scaleLinear()
+        .domain([0, max_value])
+        .range([height_visu3, 0]);
+    svg_visu3.append("g")
+        .call(d3.axisLeft(y).ticks(5))
+
+    // Label de l'axe Y
+    svg_visu3.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", -40)
+        .attr("y", -20)
+        .style("font-size", "12px")
+        .text("Temps de visionnage (heures)")
+        .attr("text-anchor", "start")
+
+
+    /*** Titre***/
 
     svg_visu3.append("text")
         .attr("x", (width_visu3 / 2) - 170)
@@ -81,39 +98,11 @@ d3.csv("data/week_activity.csv").then(function(data) {
         .style("font-size", "14px")
         .text(": Temps de visionnage cumulé hebdomadaire");
 
-    // Add X axis label:
-    svg_visu3.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width_visu3 + 40)
-        .attr("y", height_visu3 + 35)
-        .style("font-size", "12px")
-        .text("Semaine");
 
-    // Add Y axis label:
-    svg_visu3.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", -40)
-        .attr("y", -20)
-        .style("font-size", "12px")
-        .text("Temps de visionnage (heures)")
-        .attr("text-anchor", "start")
-
-    let max_value = d3.max(data, function(d) { return (parseInt(d["Both"]) + parseInt(d["Hana"]) + parseInt(d["Tarik"])); })
-
-    // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, max_value])
-        .range([height_visu3, 0]);
-    svg_visu3.append("g")
-        .call(d3.axisLeft(y).ticks(5))
+    /***** CHART ******/
 
 
-
-    //////////
-    // CHART //
-    //////////
-
-    // Add a clipPath: everything out of this area won't be drawn.
+    // définition d'un clip pour ne pas dessiner autour
     const clip = svg_visu3.append("defs").append("svg:clipPath")
         .attr("id", "clip")
         .append("svg:rect")
@@ -122,19 +111,15 @@ d3.csv("data/week_activity.csv").then(function(data) {
         .attr("x", 0)
         .attr("y", 0);
 
-
-    // Create the scatter variable: where both the circles and the brush take place
-    const areaChart = svg_visu3.append('g')
-        .attr("clip-path", "url(#clip)")
-
-    // Area generator
+    // générateur d'aire
     const area = d3.area()
         .x(function(d) { return x(d.data["Week"]); })
         .y0(function(d) { return y(d[0]); })
         .y1(function(d) { return y(d[1]); })
 
-    // Show the areas
-    areaChart
+    // Dessin des aires
+    svg_visu3.append('g')
+        .attr("clip-path", "url(#clip)")
         .selectAll("mylayers")
         .data(stackedData)
         .join("path")
@@ -143,48 +128,41 @@ d3.csv("data/week_activity.csv").then(function(data) {
         .attr("d", area)
 
 
-    //////////
-    // HIGHLIGHT GROUP //
-    //////////
+    /*** Highlight ***/
 
-    // What to do when one group is hovered
+    // Highlight le groupe sélectionné
     const highlight = function(event, d) {
-        // reduce opacity of all groups
-        d3.selectAll(".myArea").style("opacity", .1)
-            // expect the one that is hovered
-        d3.select("." + d).style("opacity", 1)
-    }
-
-    // And when it is not hovered anymore
+            d3.selectAll(".myArea").style("opacity", 0.1) //réduire l'oppacité sur les autres groupes
+            d3.select("." + d).style("opacity", 1) //augmente l'oppacité sur le groupe sélectionné
+        }
+        // Retour à l'état initial
     const noHighlight = function(event, d) {
         d3.selectAll(".myArea").style("opacity", 1)
     }
 
 
 
-    //////////
-    // LEGEND //
-    //////////
+    /*** LÉGENDE ***/
 
-    // Add one dot in the legend for each name.
+    // Ajout du carré couleur
     const size = 20
     svg_visu3.selectAll("myrect")
         .data(keys)
         .join("rect")
         .attr("x", 400)
-        .attr("y", function(d, i) { return 10 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("y", function(d, i) { return 10 + i * (size + 5) })
         .attr("width", size)
         .attr("height", size)
         .style("fill", function(d) { return color(d) })
         .on("mouseover", highlight)
         .on("mouseleave", noHighlight)
 
-    // Add one dot in the legend for each name.
+    // Ajout du texte
     svg_visu3.selectAll("mylabels")
         .data(keys)
         .join("text")
         .attr("x", 400 + size * 1.2)
-        .attr("y", function(d, i) { return 10 + i * (size + 5) + (size / 2) + 5 }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("y", function(d, i) { return 10 + i * (size + 5) + (size / 2) + 5 })
         .style("fill", function(d) { return color(d) })
         .text(function(d) { return d })
         .attr("text-anchor", "left")
